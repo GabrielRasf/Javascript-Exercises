c67cd1415873e90156e5c8ee4c28d244507a12a9
Gabriel R <gabrielrasf@hotmail.com>
2025-06-24 14:59:29 -0300

    Add initial files

---
+++ 01-NullAndUndefined/script.js
@@ -0,0 +1,5 @@
+let a
+console.log(a) // return undefined -> the variable has been declared but not defined
+
+let b = null
+console.log(b) // return null -> it is a intentional abscence of any object value
